dd59ad7384421b2c4870518271ac803e5e7e9257
Austin <me@austinwilliams.dev>
2021-03-09 17:01:27 -0800

    minor code cleanup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import "./App.css";
 import Content  from "./Content";
 import {Footer} from "./Footer";
 import {Slide}  from "./Slide";
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 function App() {
   const [animate, setAnimate] = useState(true);
-  const onAnimationEnd = () => setAnimate(false);
   const [pageNumber, setPageNumber] = useState(0);
 
   const {slideText, footerText} = Content[pageNumber];
@@ -17,16 +16,19 @@ function App() {
       <Slide
         animate={animate}
         content={slideText}
-        onAnimationEnd={onAnimationEnd}/>
+        onAnimationEnd={() => {
+          setAnimate(false);
+        }}
+      />
       <Footer
         content={footerText}
         navigateNext={() => {
-          setPageNumber(isLastPage ? 0 : pageNumber + 1);
           setAnimate(true);
+          setPageNumber(isLastPage ? 0 : pageNumber + 1);
         }}
         navigateBack={() => {
-          setPageNumber(pageNumber - 1);
           setAnimate(true);
+          setPageNumber(pageNumber - 1);
         }}
       />
     </main>
